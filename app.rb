13803464cfc389f4e0ac0f9dfe0e2cba1174f9d6
bogdan <bgdnpn@gmail.com>
2020-04-07 20:49:40 +0300

    Create proper template structure (almost)

--- app.rb
+++ app.rb
@@ -6,11 +6,28 @@ get "/" do
   erb :index
 end
 
+# get the management page
+get "/manage" do
+  erb :manage
+end
+
+# create new message
+post "/manage" do
+  nil
+end
+
+# get about page
+get "/about" do
+  erb :about
+end
+
+# get a dummy JSON, but valid response
 get "/json" do
   content_type 'application/json'
   get_file
 end
 
-def get_file(filename="simple-buzz-sample.json")
+# mock data store
+def get_file(filename="resources/simple-buzz-sample.json")
   File.read(filename)
 end
